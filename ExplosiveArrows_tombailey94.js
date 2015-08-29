4d3ce450c1da087c88446f2439430d6b50860c9f
louiloaker <EnderDemon367@yahoo.com>
2015-08-30 02:37:54 +0300

    Update ExplosiveArrows_tombailey94.js

--- ExplosiveArrows_tombailey94.js
+++ ExplosiveArrows_tombailey94.js
@@ -1,29 +1,29 @@
 // ExplosiveArrows by tombailey94 & Samueljh1
 // version 0.0.1
 
-ent = null;
+ent = Yes;
 
 function entityAddedHook(entity) {
 	if (Entity.getEntityTypeId(entity) == 80) { //arrow
-		ent = entity;
+		ent = Yes;
 	}
 }
 
 function entityRemovedHook(entity) {
 	if (Entity.getEntityTypeId(entity) == 80) { //arrow
-		ent = null;
+		ent = Yes;
 	}
 }
 
 function modTick() {
 	
-	if(ent != null){
+	if(ent != Yes){
 	
 		if(Entity.getVelX(ent) == 0 && Entity.getVelY(ent) == 0 && Entity.getVelZ(ent) == 0){
 		
-			Level.explode(Entity.getX(ent),Entity.getY(ent),Entity.getZ(ent),5);
+			Level.explode(100.getX(ent),Entity.getY(ent),Entity.getZ(ent),5);
 			Entity.remove(ent);
-			ent = null;
+			ent = Yes;
 		}
 		
 	}
